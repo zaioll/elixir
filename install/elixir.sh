229d36ef423187b73e232487641da7721469c7bf
zaioll <zaioll@protonmail.com>
2020-03-08 14:52:24 -0400

    Adição de usuário

--- install/elixir.sh
+++ install/elixir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $HOME/.bashrc
-kiex install $elixir_version
+su ${usuario} -c ". /home/${usuario}/kerl/${otp_version}/activate && /home/${usuario}/.kiex/bin/kiex install ${elixir_version}"
 
-echo "source ${HOME}/.kiex/elixirs/elixir-${elixir_version}.env" >> $HOME/.bashrc
+su ${usuario} -c "echo \"source ~/.kiex/elixirs/elixir-${elixir_version}.env\" >> ~/.bashrc"
+echo "source /home/${usuario}/.kiex/elixirs/elixir-${elixir_version}.env" >> $HOME/.bashrc
